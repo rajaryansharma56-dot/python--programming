2f191194c784320a54835daa2cdb307f16ccfb0d
Raj Aryan Sharma <rajaryansharma56@gmail.com>
2026-09-06 20:08:44 +0530

    Implement if-elif logic for sum evaluation

---
+++ if_elif_statement.py
@@ -0,0 +1,14 @@
+a=int(input("enter value of a :"))
+
+b=int(input("enter value of b :"))
+
+sum =a+b
+
+if sum>=20 or sum<10:
+    print("not good :")
+
+elif sum>=30 or sum<20:
+    print("decent ")
+
+else:
+    print("below average :")
