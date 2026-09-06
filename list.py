f85fa07fd88606bacc384ca23dd30d4be404c438
Raj Aryan Sharma <rajaryansharma56@gmail.com>
2026-09-06 20:07:13 +0530

    Add fruit input and display functionality

---
+++ list.py
@@ -0,0 +1,15 @@
+fruits=[]
+
+f1=input("enter fruit name :")
+
+fruits.append(f1)
+
+f2=input("enter fruit name :")
+
+fruits.append(f2)
+
+f3=input("enter fruit name :")
+
+fruits.append(f3)
+
+print("the fruits in the list is",fruits)
